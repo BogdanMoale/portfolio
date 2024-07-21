2307dbc56086feb6774665cf3c935176fea3e2e4
BogdanMoale <moale.bogdan.ionica@gmail.com>
2024-07-21 21:47:13 +0300

    update links to open in a new tab

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -70,6 +70,8 @@ const ProjectCard = ({
             >
               <a
                 href={github_url}
+                target="_blank"
+                rel="noopener noreferrer"
                 className="flex items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200"
               >
                 <AiFillGithub /> <span>Github</span>
